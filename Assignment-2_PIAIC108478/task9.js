1567c07b4d10273f2b20b950ed4b112d225cc3e8
Mubashir Nazir <108254736+mubashirnazir@users.noreply.github.com>
2023-02-20 20:48:43 +0500

    Update task9.js

--- Assignment-2_PIAIC108478/task9.js
+++ Assignment-2_PIAIC108478/task9.js
@@ -6,7 +6,7 @@ const string = ("character of a string");
 // console.log(string.length);
 
 //getting the string index
-var char = string[6];
+var char = string.chatAt(6);
 
 
 if (char == "a" || char == "e" ||char == "i" ||char == "o" ||char == "u") {
